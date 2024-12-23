8e20c4f785fa8358e9f628c1aa393fe95a73b396
Daniel <danielukoha101@gmail.com>
2024-12-23 19:39:51 +0100

    add lock file

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,12 +2,12 @@ const config = require('./src/config');
 
 module.exports = {
   siteMetadata: {
-    title: 'Brittany Chiang',
+    title: 'Daniel Ukoha',
     description:
-      'Brittany Chiang is a software engineer who specializes in building (and occasionally designing) exceptional digital experiences.',
+      'Daniel Ukoha is a software engineer with hands-on experience using cutting-edge technologies and up-to-date industry knowledge.',
     siteUrl: 'https://brittanychiang.com', // No trailing slash allowed!
     image: '/og.png', // Path to your image you placed in the 'static' folder
-    twitterUsername: '@bchiang7',
+    twitterUsername: '@i_amsuperfly',
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
@@ -20,8 +20,8 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: 'Brittany Chiang',
-        short_name: 'Brittany Chiang',
+        name: 'Daniel Ukoha',
+        short_name: 'Daniel Ukoha',
         start_url: '/',
         background_color: config.colors.darkNavy,
         theme_color: config.colors.navy,
